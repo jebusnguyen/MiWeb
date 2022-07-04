0e34b1dfbccbd1e0cd38ae06c50055f397aef7da
Jebusnguyen <jsalazacar22@gmail.com>
2022-07-03 21:04:09 -0500

    se agregaron apuntes de mysql y se crearon los apuntes de git

--- Ejercicios/mysql/Funciones basicas.sql
+++ Ejercicios/mysql/Funciones basicas.sql
@@ -1,29 +1,37 @@
 -- create database holamundo;
 use holamundo;
 -- create table Animales(
--- id int auto_increment, 
--- tipo varchar(255),
+-- id int auto_increment, el id siempre necesita la funcion autoincrement y siempre llevara un valor de numero entero
+-- tipo varchar(255), 
 -- estado varchar(255),
--- primary key (id),
+-- primary key (id), asi definimos la primar key de una lista
 -- );
 
 -- alter table Animales modify colum id int auto_increment;
+-- aqui esta solicitando alterar la tabla modificando la columna id
+-- y agregar la propiedad autoincrement (asumiendo que no la tiene claro)
 
 insert into Animales (tipo, estado) values ('chanchito', 'feliz');
 insert into Animales (tipo, estado) values ('dragon', 'feliz');
 insert into Animales (tipo, estado) values ('felipe', 'triste');
+-- esta es la forma de agregar datos a una tabla
 
 select * from Animales;
 select * from Animales where id=1;
 select * from Animales where estado= 'feliz' and tipo= 'chanchito';
 
 update Animales set estado= 'feliz' where id= 5;
+-- esta funcion sirve para actualizar datos de una columna en este caso
+-- modifica la propiedad feliz del elemento con id =5
 
 select * from Animales where id=5;
 
 delete from Animales where estado= 'feliz';
 -- Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column.  To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.
-
+-- Este es un control de seguridad para no borrar accidentalmente datos 
+-- se puede desconfigurar pero no es recomendable. consiste en oligar
+-- al sistema a ejecutar delete o update exclusivamente si mencionas a la columna
+-- id
 
 select * from Animales; 
 delete from Animales where id=1;
